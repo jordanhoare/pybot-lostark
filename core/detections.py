e8b010c16912b3998d0d12a78f2d55581d936f97
jordanhoare <jordanhoare0@gmail.com>
2022-03-01 09:53:20 +1000

    improving oop for fishing module

---
+++ core/detections.py
@@ -0,0 +1,125 @@
+import os
+import sys
+import time
+from random import randint
+from time import sleep
+
+import cv2 as cv
+import numpy as np
+import pyautogui
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+
+from core.vision import Vision
+from core.window_capture import WindowCapture
+
+######
+######
+######
+######
+######
+######
+######
+######
+######
+######
+######
+
+# initialize the capture classes and counters
+window_title = "Lost Ark"
+wincap = WindowCapture(window_title)
+nibble_image = Vision("data/nibble_image.jpeg")
+low_energy_image = Vision("data/low_energy_image.jpeg")
+life_skill_icon = Vision("data/life_skill_check.jpeg")
+floater_image = Vision("data/floater_image.jpeg")
+no_durability_image = Vision("data/no_durability.jpeg")
+start_time = time.time()  # start time of the loop
+idle_timer = time.time()
+catch_counter = 0
+
+
+def Check_LifeSkill():
+    print(f">>> Switching to Trade Skill Quickslot.")
+    sleep(randint(0, 1))
+    pyautogui.click(750, 750)
+    pyautogui.press("b")
+    sleep(randint(2, 3))
+    pyautogui.click(750, 750)
+    pyautogui.press("e")
+    sleep(randint(2, 3))
+
+
+def EnergyCondition():
+    if len(low_energy_rectangles) > 0:
+        print(f">>> Stopping script due to low energy.")
+        return True
+    else:
+        return False
+
+
+def idle_check():
+    print(f">>> Player has been idling for too long")
+    print(f">>> Restarting loop.")
+    sleep(randint(0, 1))
+    pyautogui.click(750, 750)
+    pyautogui.press("e")
+    return False
+
+
+def catch_fish():
+    pyautogui.press("e")
+    sleep(randint(6, 7))
+    pyautogui.click(752, 751)
+    sleep(randint(0, 1))
+    pyautogui.press("e")
+    idle_timer = time.time()  # start time of the loop
+
+
+def end_process():
+    cv.destroyAllWindows()
+    stop_time = time.time()
+    print(f">>> Successful fishing attempts: '{catch_counter}' times.")
+    print(
+        f">>> Script finished after:",
+        "{:.3f}".format(1.0 / (stop_time - start_time)),
+    )
+    print(f"####################################")
+
+
+while True:
+    start = time.time()  # start time of the loop
+    screenshot = wincap.get_screenshot()  # get an updated image of the game
+
+    # render object detection images
+    nibble_rectangles = nibble_image.find(screenshot, 0.75)
+    life_skill_activated = life_skill_icon.find(screenshot, 0.88)
+    low_energy_rectangles = low_energy_image.find(screenshot, 0.96)
+    floater_rectangles = floater_image.find(screenshot, 0.90)
+    no_durability_check = no_durability_image.find(screenshot, 0.90)
+
+    # draw the detection results onto the original image
+    output_image = nibble_image.draw_rectangles(screenshot, nibble_rectangles)
+
+    # display the processed image
+    output_image = cv.resize(output_image, (960, 540))
+    cv.imshow("Lost Ark:  Fishing automation", output_image)
+
+    # nibble check
+    if len(nibble_rectangles) > 0:
+        catch_fish()
+        catch_counter = catch_counter + 1
+    elif len(life_skill_activated) < 1:
+        Check_LifeSkill()
+    elif len(no_durability_check) > 0:
+        print(f">>> No durability left on your tool.")
+        try:
+            end_process()
+        except Exception as Ex:
+            print(Ex)
+        break
+    elif cv.waitKey(1) == ord("=") or EnergyCondition():
+        try:
+            end_process()
+        except Exception as Ex:
+            print(Ex)
+        break
--- core/test.py
+++ core/test.py
@@ -1,16 +1,13 @@
 import os
 import sys
 import time
-from ast import While
 from asyncore import loop
-from pickle import TRUE
 from random import randint
 from time import sleep
 
 import cv2 as cv
 import numpy as np
 import pyautogui
-from sqlalchemy import false
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 from core.vision import Vision
@@ -35,27 +32,23 @@ wincap = WindowCapture(window_title)
 nibble_icon = Vision("data/nibble_icon.jpeg")
 low_energy = Vision("data/low_energy.jpeg")
 catch_counter = 0
-
-# bot actions
-sleep(randint(0, 1))
-pyautogui.click(750, 750)
-pyautogui.press("e")
 start_time = time.time()  # start time of the loop
 idle_timer = time.time()
-
-low_energy_check = False
-
-
-def le_fish():
-    while True:
-        """
-        all true for it to fish
-        """
-        fishing()
+start = time.time()
 
 
 def fishing():
-    print("Init")
+    start = time.time()  # start time of the loop
+    screenshot = wincap.get_screenshot()  # get an updated image of the game
+    nibble_rectangles = nibble_icon.find(screenshot, 0.75)  # do object detection
+    low_energy_rectangles = low_energy.find(screenshot, 0.92)  # do object detection
+    output_image = nibble_icon.draw_rectangles(
+        screenshot, nibble_rectangles
+    )  # draw the detection results onto the original image
+
+    # display the processed image
+    output_image = cv.resize(output_image, (960, 540))
+    cv.imshow("Lost Ark:  Fishing automation", output_image)
 
     # nibble check
     if len(nibble_rectangles) > 0:
@@ -67,23 +60,17 @@ def fishing():
         pyautogui.press("e")
         idle_timer = time.time()  # start time of the loop
 
-        print("Nibble Check")
-    print("END")
-
 
 def end_process():
-    try:
-        cv.destroyAllWindows()
-        stop_time = time.time()
-        total_time = stop_time - start_time
-        print(f">>> Successful fishing attempts: '{catch_counter}' times.")
-        print(
-            f">>> Script finished after:",
-            "{:.3f}".format(1.0 / (start - idle_timer)),
-        )
-        print(f"####################################")
-    except Exception as Ex:
-        print(Ex)
+    cv.destroyAllWindows()
+    stop_time = time.time()
+    total_time = stop_time - start_time
+    print(f">>> Successful fishing attempts: '{catch_counter}' times.")
+    print(
+        f">>> Script finished after:",
+        "{:.3f}".format(1.0 / (start - idle_timer)),
+    )
+    print(f"####################################")
 
 
 def idle_check():
@@ -94,17 +81,33 @@ def idle_check():
         return False
 
 
-while True:
-    start = time.time()
-    screenshot = wincap.get_screenshot()
-    nibble_rectangles = nibble_icon.find(screenshot, 0.75)
-    low_energy_rectangles = low_energy.find(screenshot, 0.92)
-    output_image = nibble_icon.draw_rectangles(screenshot, nibble_rectangles)
-    # display the processed image
+def EnergyCondition():
+    if len(low_energy_rectangles) > 0:
+        print(f">>> Stopping script due to low energy.")
+        return False
+    else:
+        return True
 
-    output_image = cv.resize(output_image, (960, 540))
-    cv.imshow("Lost Ark:  Fishing automation", output_image)
 
-    if cv.waitKey(1) == ord("="):
-        end_process()
-        break
+def le_fish():
+    # bot actions
+    sleep(randint(0, 1))
+    pyautogui.click(750, 750)
+    pyautogui.press("e")
+    start_time = time.time()  # start time of the loop
+    idle_timer = time.time()
+
+    while True:
+        """
+        all true for it to fish
+        """
+        fishing()
+
+        if idle_check():
+            fishing()
+        else:
+            end_process()
+            break
+
+
+le_fish()
