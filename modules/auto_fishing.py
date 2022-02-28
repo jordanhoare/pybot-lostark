1bd119c13a988472cab75c56fa87bd13df5073cc
jordanhoare <jordanhoare0@gmail.com>
2022-02-28 15:13:41 +1000

    Resizing stream window to save FPS and resources

--- modules/auto_fishing.py
+++ modules/auto_fishing.py
@@ -14,30 +14,25 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 from core.vision import Vision
 from core.window_capture import WindowCapture
 
-# properties
+# initialize the capture classes
 window_title = "Lost Ark"
-
-# initialize the WindowCapture class
 wincap = WindowCapture(window_title)
-
-# initialize the Vision class
 fishing_icon = Vision("data/fishing_icon.jpeg")
 
 while True:
-
     start_time = time.time()  # start time of the loop
-
     # get an updated image of the game
     screenshot = wincap.get_screenshot()
 
-    # do object detectione
+    # do object detection
     rectangles = fishing_icon.find(screenshot, 0.75)
 
     # draw the detection results onto the original image
     output_image = fishing_icon.draw_rectangles(screenshot, rectangles)
 
     # display the processed image
-    cv.imshow("Matches", output_image)
+    output_image = cv.resize(output_image, (960, 540))
+    cv.imshow("Lost Ark:  Fishing automation", output_image)
 
     # take bot actions
     if len(rectangles) > 0:
