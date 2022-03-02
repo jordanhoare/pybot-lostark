f7667746ac1e2888cea503aa78c7060b7c28d20f
jordanhoare <jordanhoare0@gmail.com>
2022-03-02 10:47:56 +1000

    folder restructuring

--- pybot_lostark/fishing_bot/config.py
+++ pybot_lostark/fishing_bot/config.py
@@ -1,11 +1,17 @@
 from functools import lru_cache
+
 from pydantic import BaseSettings
 
+# life_skill_activated = life_skill_icon.find(screenshot, 0.90)
 
-class Settings(BaseSettings):
-    ENERGY_THRESHOLD: int = 0.9
+
+class Template(BaseSettings):
+    NIBBLE_NEEDLE: str = "data/low_energy_image.jpeg"
+    NIBBLE_THRESHOLD: int = 0.80
+    DURABILITY_NEEDLE: str = "data/low_energy_image.jpeg"
+    DURABILITY_THRESHOLD: int = 0.80
 
 
 @lru_cache
 def get_settings():
-    return Settings()
+    return Template()
